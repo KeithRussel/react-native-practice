f30e6c5e69fbefcc0b765282fe75504368fef209
KeithRussel <keithmaniacop@gmail.com>
2022-02-22 22:45:08 +0800

    React Navigation V6 - Tab Navigator, Material Tab at Bottom & Top

---
+++ src/ScreenA.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import type {Node} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+export default function ScreenA({navigation}) {
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+  };
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen A</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({pressed}) => ({backgroundColor: pressed ? '#ddd' : '#0f0'})}>
+        <Text style={styles.text}>Go to Screen B</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+});
