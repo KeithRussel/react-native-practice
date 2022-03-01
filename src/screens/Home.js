8bcaa2ce1a0cb042776ecc7a58024b0bd3cb0725
KeithRussel <keithmaniacop@gmail.com>
2022-03-01 20:35:01 +0800

    Local & Schedule Push Notification

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,7 +13,8 @@ import CustomButton from '../utils/CustomButton';
 import SQLite from 'react-native-sqlite-storage';
 import {useSelector, useDispatch} from 'react-redux';
 import {setName, setAge, increaseAge, getCities} from '../redux/actions';
-import {FlatList} from 'react-native-gesture-handler';
+import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
+import PushNotification from 'react-native-push-notification';
 
 const db = SQLite.openDatabase(
   {
@@ -111,6 +112,30 @@ export default function Home({navigation, route}) {
     }
   };
 
+  const handleNotification = (item, index) => {
+    PushNotification.cancelAllLocalNotifications();
+
+    PushNotification.localNotification({
+      channelId: 'test-channel',
+      title: 'You clicked on ' + item.country,
+      message: item.city,
+      bigText:
+        item.city +
+        ' is one of the largest and most beautiful cities in ' +
+        item.country,
+      color: 'red',
+      id: index,
+    });
+
+    PushNotification.localNotificationSchedule({
+      channelId: 'test-channel',
+      title: 'Alarm',
+      message: 'You clicked on ' + item.country + ' 20 seconds ago',
+      date: new Date(Date.now() + 20 * 1000),
+      allowWhileIdle: true,
+    });
+  };
+
   return (
     <View style={styles.body}>
       <Text style={[GlobalStyle.CustomFont, styles.text]}>
@@ -118,11 +143,16 @@ export default function Home({navigation, route}) {
       </Text>
       <FlatList
         data={cities}
-        renderItem={({item}) => (
-          <View style={styles.item}>
-            <Text style={styles.title}>{item.country}</Text>
-            <Text style={styles.subtitle}>{item.city}</Text>
-          </View>
+        renderItem={({item, index}) => (
+          <TouchableOpacity
+            onPress={() => {
+              handleNotification(item, index);
+            }}>
+            <View style={styles.item}>
+              <Text style={styles.title}>{item.country}</Text>
+              <Text style={styles.subtitle}>{item.city}</Text>
+            </View>
+          </TouchableOpacity>
         )}
         keyExtractor={(item, index) => index.toString()}
       />
